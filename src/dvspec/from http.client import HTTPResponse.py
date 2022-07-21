0adbd0d49a272ca0dd3f7397f2f520bd9e762542
Roberto Saltini <saltini.roberto@gmail.com>
2022-07-22 01:27:41 +1000

    Removed unwanted file

--- src/dvspec/from http.client import HTTPResponse.py
+++
@@ -1,9 +0,0 @@
-from http.client import HTTPResponse
-import string
-
-
-async def get_request(url: string) -> HTTPResponse
-
-async def foo():
-    response1 = get_request("http://..../fork")
-    response2 = get_request("http://..../attestation_data")
